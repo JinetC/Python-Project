7a2dcc64286b0c4b03f5ef733ec020b18abb3725
Jineth Hettiarachchi <110524681+JinetC@users.noreply.github.com>
2022-09-25 20:32:05 +0530

    Add files via upload

---
+++ DS/e1.py
@@ -0,0 +1,19 @@
+import datetime
+datetime.datetime.now()
+print("The date and time is:",datetime.datetime.now())
+x = 10
+y = 12
+z = x + y
+print(z)
+list [35.9, 25, 'Celsius']
+student_grades = [9.1, 8.8, 7.5]
+mysum = sum(student_grades)
+leanth = len(student_grades)
+mean = mysum/leanth
+print(mean)
+
+
+
+
+
+
